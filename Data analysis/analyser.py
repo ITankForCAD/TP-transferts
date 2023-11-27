d31389a67c50a1230cb56212e311152eae57bc7a
craqu <Antoine.veillette@gmail.com>
2023-11-27 12:58:06 -0500

    petit details

--- Data analysis/analyser.py
+++ Data analysis/analyser.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import LinearLocator
 from mpl_toolkits.mplot3d import Axes3D
 
 
-data = np.loadtxt("C++\data.txt")
+data = np.loadtxt("/Users/antoine/Desktop/GPH/A2023/TransfertTermique/TP/TP-transferts/C++/data.txt")
 x = np.linspace(0,data.shape[1],num=data.shape[1])
 t = np.linspace(0,data.shape[0],num=data.shape[0])
 X,T = np.meshgrid(x,t)
@@ -15,7 +15,7 @@ fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 surface = ax.plot_surface(T, X, data, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-ax.set_zlim(300, 900)
+ax.set_zlim(400, 1200)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter("{x:.2f}")
 ax.tick_params(axis='z', labelsize='smaller')
@@ -24,7 +24,7 @@ ax.tick_params(axis='y', labelsize='smaller')
 fig.colorbar(surface, shrink=0.5, aspect=5, location="bottom")
 ax.set_xlabel("t [s]")
 ax.set_ylabel("x [m]")
-ax.set_zlabel("Temp [$\degree C $]", labelpad=20)
+ax.set_zlabel("Temp [$\degree K $]", labelpad=20)
 ax.zaxis.set_rotate_label(False)
 plt.show()
 

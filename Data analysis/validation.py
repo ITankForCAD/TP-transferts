669aca007a831d02e358ff7bbcf6cd9e1be66f76
Thomas Desmarais <thomas-r.desmarais.1@ulaval.ca>
2023-11-27 12:57:26 -0500

    ptit save rapide

--- Data analysis/validation.py
+++ Data analysis/validation.py
@@ -4,7 +4,10 @@ import pandas as pd
 
 from data_cleanup import Nettoyeur
 
-
+filepath = "Data analysis\\data.csv"
 
 # TODO fonctions pour comparer nos résultats avec les données expérimentales
+
+data = Nettoyeur(filepath)
+
 
